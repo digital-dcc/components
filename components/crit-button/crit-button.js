6cbae179d6986445b2c1433380e528bdee48fd1c
Richard Walker <digitalsadhu@gmail.com>
2024-07-21 20:23:50 +1200

    add missing class definitions

--- components/crit-button/crit-button.js
+++ components/crit-button/crit-button.js
@@ -25,7 +25,78 @@ const classInfo = {
     9: {multiplier: 2, die: 20, table: 'V'},
     10: {multiplier: 2, die: 20, table: 'V'},
   },
-  // add other classes here...
+  wizard: {
+    1: {multiplier: 1, die: 6, table: 'I'},
+    2: {multiplier: 1, die: 6, table: 'I'},
+    3: {multiplier: 1, die: 8, table: 'I'},
+    4: {multiplier: 1, die: 8, table: 'I'},
+    5: {multiplier: 1, die: 10, table: 'I'},
+    6: {multiplier: 1, die: 10, table: 'I'},
+    7: {multiplier: 1, die: 12, table: 'I'},
+    8: {multiplier: 1, die: 12, table: 'I'},
+    9: {multiplier: 1, die: 14, table: 'I'},
+    10: {multiplier: 1, die: 14, table: 'I'},
+  },
+  cleric: {
+    1: {multiplier: 1, die: 8, table: 'III'},
+    2: {multiplier: 1, die: 8, table: 'III'},
+    3: {multiplier: 1, die: 10, table: 'III'},
+    4: {multiplier: 1, die: 10, table: 'III'},
+    5: {multiplier: 1, die: 12, table: 'III'},
+    6: {multiplier: 1, die: 12, table: 'III'},
+    7: {multiplier: 1, die: 14, table: 'III'},
+    8: {multiplier: 1, die: 14, table: 'III'},
+    9: {multiplier: 1, die: 16, table: 'III'},
+    10: {multiplier: 1, die: 16, table: 'III'},
+  },
+  thief: {
+    1: {multiplier: 1, die: 10, modifier: 0, table: 'II'},
+    2: {multiplier: 1, die: 12, modifier: 0, table: 'II'},
+    3: {multiplier: 1, die: 14, modifier: 0, table: 'II'},
+    4: {multiplier: 1, die: 16, modifier: 0, table: 'II'},
+    5: {multiplier: 1, die: 20, modifier: 0, table: 'II'},
+    6: {multiplier: 1, die: 24, modifier: 0, table: 'II'},
+    7: {multiplier: 1, die: 30, modifier: 0, table: 'II'},
+    8: {multiplier: 1, die: 30, modifier: 2, table: 'II'},
+    9: {multiplier: 1, die: 30, modifier: 4, table: 'II'},
+    10: {multiplier: 1, die: 30, modifier: 6, table: 'II'},
+  },
+  dwarf: {
+    1: {multiplier: 1, die: 10, table: 'III'},
+    2: {multiplier: 1, die: 12, table: 'III'},
+    3: {multiplier: 1, die: 14, table: 'III'},
+    4: {multiplier: 1, die: 16, table: 'IV'},
+    5: {multiplier: 1, die: 20, table: 'IV'},
+    6: {multiplier: 1, die: 24, table: 'V'},
+    7: {multiplier: 1, die: 30, table: 'V'},
+    8: {multiplier: 1, die: 30, table: 'V'},
+    9: {multiplier: 2, die: 20, table: 'V'},
+    10: {multiplier: 2, die: 20, table: 'V'},
+  },
+  halfling: {
+    1: {multiplier: 1, die: 8, table: 'III'},
+    2: {multiplier: 1, die: 8, table: 'III'},
+    3: {multiplier: 1, die: 10, table: 'III'},
+    4: {multiplier: 1, die: 10, table: 'III'},
+    5: {multiplier: 1, die: 12, table: 'III'},
+    6: {multiplier: 1, die: 12, table: 'III'},
+    7: {multiplier: 1, die: 14, table: 'III'},
+    8: {multiplier: 1, die: 14, table: 'III'},
+    9: {multiplier: 1, die: 16, table: 'III'},
+    10: {multiplier: 1, die: 16, table: 'III'},
+  },
+  elf: {
+    1: {multiplier: 1, die: 6, table: 'II'},
+    2: {multiplier: 1, die: 8, table: 'II'},
+    3: {multiplier: 1, die: 8, table: 'II'},
+    4: {multiplier: 1, die: 10, table: 'II'},
+    5: {multiplier: 1, die: 10, table: 'II'},
+    6: {multiplier: 1, die: 12, table: 'II'},
+    7: {multiplier: 1, die: 12, table: 'II'},
+    8: {multiplier: 1, die: 14, table: 'II'},
+    9: {multiplier: 1, die: 14, table: 'II'},
+    10: {multiplier: 1, die: 16, table: 'II'},
+  },
 };
 
 export class CritButton extends LitElement {
@@ -100,6 +171,12 @@ export class CritButton extends LitElement {
     if (this.luckySignSlug === 'warriors-arm') {
       mod = mod * 2;
     }
+
+		// the thief has a modifier at higher levels
+		if (classInfo[this.class][this.level].modifier) {
+			mod = mod + classInfo[this.class][this.level].modifier;
+		}
+
     if (this.modifierOverride) mod = Number(this.modifierOverride);
     return mod;
   }
