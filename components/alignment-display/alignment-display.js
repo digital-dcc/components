c65a755f783712017926d3905b31a4199e1ac659
Richard Walker <digitalsadhu@gmail.com>
2024-07-21 15:22:19 +1200

    add alignment-display

---
+++ components/alignment-display/alignment-display.js
@@ -0,0 +1,67 @@
+import {LitElement, html, css} from 'lit';
+
+export class AlignmentDisplay extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        padding: 0px;
+      }
+      .wrapper {
+        border-radius: 5px;
+        border: 1px black solid;
+        display: flex;
+				flex-direction: column;
+        min-height: 25px;
+        font-family: var(--main-font, 'Arial', sans-serif);
+        font-size: 1em;
+				align-items: center;
+				min-width: fit-content;
+				max-width: 120px;
+				padding: 10px 0px;
+      }
+			h1 {
+				margin: 0;
+				padding: 0;
+			}
+			.alignment-name {
+				text-align: center;
+			}
+    `;
+  }
+
+  static get properties() {
+    return {
+      alignment: {type: String},
+    };
+  }
+
+  constructor() {
+    super();
+    this.alignment = null;
+  }
+
+	get icon() {
+		if (this.alignment === 'law') {
+			return html`placeholder law`;
+		}
+		if (this.alignment === 'neutrality') {
+			return html`placeholder neutrality`;
+		}
+		if (this.alignment === 'chaos') {
+			return html`placeholder chaos`;
+		}
+		return ``;
+	}
+
+  render() {
+    return html`
+      <div part="wrapper" class="wrapper">
+        <div class="alignment-icon" part="alignment-icon">${this.icon}</div>
+        <h1 class="alignment-name" part="alignment-name">${this.alignment}</h1>
+      </div>
+    `;
+  }
+}
+
+window.customElements.define('alignment-display', AlignmentDisplay);
---
+++ components/alignment-display/alignment-display.test.js
@@ -0,0 +1,37 @@
+//@ts-nocheck
+
+import {AlignmentDisplay} from './alignment-display.js';
+// import {assert} from '@open-wc/testing';
+import {fixture, assert} from '@open-wc/testing';
+import {html} from 'lit/static-html.js';
+
+suite('alignment-display', () => {
+  test('is defined', () => {
+    const el = document.createElement('alignment-display');
+    assert.instanceOf(el, AlignmentDisplay);
+  });
+
+  test('displays correct alignment - law', async () => {
+    const el = await fixture(
+      html`<alignment-display alignment="law"></alignment-display>`
+    );
+    const alignment = el.shadowRoot?.querySelector('.alignment-name');
+    assert.match(alignment.innerText, /law/);
+  });
+
+  test('displays correct alignment - neutrality', async () => {
+    const el = await fixture(
+      html`<alignment-display alignment="neutrality"></alignment-display>`
+    );
+    const alignment = el.shadowRoot?.querySelector('.alignment-name');
+    assert.match(alignment.innerText, /neutrality/);
+  });
+
+  test('displays correct alignment - chaos', async () => {
+    const el = await fixture(
+      html`<alignment-display alignment="chaos"></alignment-display>`
+    );
+    const alignment = el.shadowRoot?.querySelector('.alignment-name');
+    assert.match(alignment.innerText, /chaos/);
+  });
+});
