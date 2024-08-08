cc8d0d4a6b492f3b71ead10ee4f63287a05e0c7c
Richard Walker <digitalsadhu@gmail.com>
2024-08-08 18:29:27 +1200

    refactor: clean up birth-augur component

---
+++ utilities/birth-augur.js
@@ -0,0 +1,125 @@
+export const birthAugurs = new Map([
+  ['harsh-winter', {birthAugur: 'Harsh winter', luckyRoll: 'All attack rolls'}],
+  ['the-bull', {birthAugur: 'The bull', luckyRoll: 'Melee attack rolls'}],
+  [
+    'fortunate-date',
+    {birthAugur: 'Fortunate date', luckyRoll: 'Missile fire attack rolls'},
+  ],
+  [
+    'raised-by-wolves',
+    {birthAugur: 'Raised by wolves', luckyRoll: 'Unarmed attack rolls'},
+  ],
+  [
+    'conceived-on-horseback',
+    {birthAugur: 'Conceived on horseback', luckyRoll: 'Mounted attack rolls'},
+  ],
+  [
+    'born-on-the-battlefield',
+    {birthAugur: 'Born on the battlefield', luckyRoll: 'Damage rolls'},
+  ],
+  [
+    'path-of-the-bear',
+    {birthAugur: 'Path of the bear', luckyRoll: 'Melee damage rolls'},
+  ],
+  ['hawkeye', {birthAugur: 'Hawkeye', luckyRoll: 'Missile fire damage rolls'}],
+  [
+    'pack-hunter',
+    {
+      birthAugur: 'Pack hunter',
+      luckyRoll: 'Attack and damage rolls for 0-level starting weapon',
+    },
+  ],
+  [
+    'born-under-the-loom',
+    {
+      birthAugur: 'Born under the loom',
+      luckyRoll: 'Skill checks (including thief skills)',
+    },
+  ],
+  [
+    'foxs-cunning',
+    {birthAugur: 'Fox’s cunning', luckyRoll: 'Find/disable traps'},
+  ],
+  [
+    'four-leafed-clover',
+    {birthAugur: 'Four-leafed clover', luckyRoll: 'Find secret doors'},
+  ],
+  ['seventh-son', {birthAugur: 'Seventh son', luckyRoll: 'Spell checks'}],
+  [
+    'the-raging-storm',
+    {birthAugur: 'The raging storm', luckyRoll: 'Spell damage'},
+  ],
+  [
+    'righteous-heart',
+    {birthAugur: 'Righteous heart', luckyRoll: 'Turn unholy checks'},
+  ],
+  [
+    'survived-the-plague',
+    {
+      birthAugur: 'Survived the plague',
+      luckyRoll: 'Magical healing',
+      extra: {
+        cleric: 'Applies to all healing performed.',
+        all: 'Applies to all magical healing received.',
+      },
+    },
+  ],
+  ['lucky-sign', {birthAugur: 'Lucky sign', luckyRoll: 'Saving throws'}],
+  [
+    'guardian-angel',
+    {birthAugur: 'Guardian angel', luckyRoll: 'Savings throws to escape traps'},
+  ],
+  [
+    'survived-a-spider-bite',
+    {
+      birthAugur: 'Survived a spider bite',
+      luckyRoll: 'Saving throws against poison',
+    },
+  ],
+  [
+    'struck-by-lightning',
+    {birthAugur: 'Struck by lightning', luckyRoll: 'Reflex saving throws'},
+  ],
+  [
+    'lived-through-famine',
+    {birthAugur: 'Lived through famine', luckyRoll: 'Fortitude saving throws'},
+  ],
+  [
+    'resisted-temptation',
+    {birthAugur: 'Resisted temptation', luckyRoll: 'Willpower saving throws'},
+  ],
+  ['charmed-house', {birthAugur: 'Charmed house', luckyRoll: 'Armor Class'}],
+  [
+    'speed-of-the-cobra',
+    {birthAugur: 'Speed of the cobra', luckyRoll: 'Initiative'},
+  ],
+  [
+    'bountiful-harvest',
+    {
+      birthAugur: 'Bountiful harvest',
+      luckyRoll: 'Hit points (applies at each level)',
+    },
+  ],
+  [
+    'warriors-arm',
+    {
+      birthAugur: 'Warrior’s arm',
+      luckyRoll: 'Critical hit tables',
+      extra: {all: 'Modifier is doubled for the purpose of crits.'},
+    },
+  ],
+  ['unholy-house', {birthAugur: 'Unholy house', luckyRoll: 'Corruption rolls'}],
+  [
+    'the-broken-star',
+    {
+      birthAugur: 'The Broken Star',
+      luckyRoll: 'Fumbles',
+      extra: {all: 'Modifier is doubled for the purpose of fumbles.'},
+    },
+  ],
+  ['birdsong', {birthAugur: 'Birdsong', luckyRoll: 'Number of languages'}],
+  [
+    'wild-child',
+    {birthAugur: 'Wild child', luckyRoll: 'Speed (each +1/-1 = +5’/-5’ speed)'},
+  ],
+]);
